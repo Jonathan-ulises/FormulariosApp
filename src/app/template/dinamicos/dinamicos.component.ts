b85c62a160ac8dd33498ee54f1d5fecfb7872ad8
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-08-16 02:23:29 -0500

    Seccion 16: Formularios - Template y Lazyload
    Capitulo 241: Agregar juagos favoritos
    En este capitulo se crearon los juegos para agregar elementos inputs en
    la lista de favoritos. esto con ngModel y con sus metodos
    correspondientes.

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -31,8 +31,20 @@ export class DinamicosComponent {
     ]
   }
 
+  nuevoJuego: string = '';
+
   guardar() {
     console.log('formulario posteado')
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+
+    this.persona.favoritos.push( {...nuevoFavorito} );
+    this.nuevoJuego = '';
   }
 
   eliminar( index: number) {
